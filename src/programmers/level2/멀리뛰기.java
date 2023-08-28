fd96892a3fe26c7f109d3047a23e60020f53df22
Shim Seongbo <107231837+ShimSeongbo@users.noreply.github.com>
2023-08-28 13:57:14 +0900

    230828

---
+++ src/programmers/level2/멀리뛰기.java
@@ -0,0 +1,19 @@
+public class 멀리뛰기 {
+    public long solution(int n) {
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        }
+        
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(4)); // Should print 5
+        System.out.println(s.solution(3)); // Should print 3
+    }
+}
