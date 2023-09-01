c0343d7004cf726b4c8ea1582475d9eec31915b7
shimseongbo <seong265@icloud.com>
2023-09-01 19:42:55 +0900

    230901

--- src/programmers/level2/연속부분수열합의개수.java
+++ src/programmers/level2/연속부분수열합의개수.java
@@ -1,8 +1,24 @@
 package programmers.level2;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class 연속부분수열합의개수 {
     public int solution(int[] elements) {
-        int answer = 0;
-        return answer;
+        Set<Integer> uniqueSums = new HashSet<>();
+        int n = elements.length;
+
+        // 원형 수열이기 때문에 시작 인덱스를 0부터 n-1까지 검사
+        for (int start = 0; start < n; start++) {
+            int sum = 0;
+
+            // 연속 부분 수열의 길이를 1부터 n까지 검사
+            for (int length = 1; length <= n; length++) {
+                sum += elements[(start + length - 1) % n]; // 원형 수열의 연속 합 계산
+                uniqueSums.add(sum); // 합을 HashSet에 저장 (중복은 자동으로 제거됨)
+            }
+        }
+
+        return uniqueSums.size(); // 유일한 합의 개수를 반환
     }
 }
