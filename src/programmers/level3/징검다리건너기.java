a53a25f6b3857c93f8e78143d0e9d6d61094158b
shimseongbo <seong265@icloud.com>
2023-10-12 10:14:47 +0900

    231012

---
+++ src/programmers/level3/징검다리건너기.java
@@ -0,0 +1,30 @@
+package programmers.level3;
+
+public class 징검다리건너기 {
+    public int solution(int[] stones, int k) {
+        int left = 1, right = 200000000, mid;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (canCross(stones, k, mid)) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return right;
+    }
+
+    private boolean canCross(int[] stones, int k, int mid) {
+        int count = 0;
+        for (int stone : stones) {
+            if (stone - mid < 0) {
+                count++;
+                if (count == k) return false;
+            } else {
+                count = 0;
+            }
+        }
+        return true;
+    }
+
+}
