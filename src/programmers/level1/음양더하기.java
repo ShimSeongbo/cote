485ccb8ef93245b0e05ec8b4c8061c2c0c29025d
shimseongbo <seong265@icloud.com>
2023-10-25 11:19:42 +0900

    231025

--- src/programmers/level1/음양더하기.java
+++ src/programmers/level1/음양더하기.java
@@ -1,10 +1,18 @@
 package programmers.level1;
 
-import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class 음양더하기 {
 
-    public int solution(int[] absolutes, boolean[] signs) {
-        return Arrays.stream(absolutes).map(i -> i * (signs[i] ? 1 : -1)).sum();
+    public static int solution(int[] absolutes, boolean[] signs) {
+        return IntStream.range(0, absolutes.length)
+                .map(i -> absolutes[i] * (signs[i] ? 1 : -1))
+                .sum();
+    }
+
+    public static void main(String[] args) {
+        int solution = solution(new int[]{4, 7, 12}, new boolean[]{true, false, true});
+
+        System.out.println("solution = " + solution);
     }
 }
