2a1070d111adc183ec49fb9705da07ad6faec7fb
shimseongbo <seong265@icloud.com>
2023-10-28 13:07:05 +0900

    231028

---
+++ src/programmers/level1/문자열다루기기본.java
@@ -0,0 +1,18 @@
+package programmers.level1;
+
+public class 문자열다루기기본 {
+
+    public boolean solution(String s) {
+
+        if (s.length() == 4 || s.length() == 6) {
+            try {
+                Integer.parseInt(s);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+
+        return false;
+    }
+}
