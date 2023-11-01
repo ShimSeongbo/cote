6baf15ce6133746e5c15291af619a71eeff84bb5
shimseongbo <seong265@icloud.com>
2023-11-01 10:21:40 +0900

    231101

---
+++ src/programmers/level1/최소직사각형.java
@@ -0,0 +1,12 @@
+package programmers.level1;
+
+public class 최소직사각형 {
+
+    public int solution(int[][] sizes) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+}
---
+++ src/programmers/level1/크기가작은부분문자열.java
@@ -0,0 +1,16 @@
+package programmers.level1;
+
+public class 크기가작은부분문자열 {
+
+    public int solution(String t, String p) {
+        int answer = 0;
+        int pLength = p.length();
+        char[] chars = t.toCharArray();
+        int charsLength = chars.length;
+
+
+
+
+        return answer;
+    }
+}
