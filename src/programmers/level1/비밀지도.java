b123c2c25e2baa529a825e89c303cb76f81202fa
shimseongbo <seong265@icloud.com>
2023-11-04 10:52:06 +0900

    231104

---
+++ src/programmers/level1/비밀지도.java
@@ -0,0 +1,12 @@
+package programmers.level1;
+
+public class 비밀지도 {
+
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+
+
+
+        return answer;
+    }
+}
